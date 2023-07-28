244ab5215bcfb5fa2174497213fffba85dccd51e
anyaguuu <ag156@rice.edu>
2023-07-28 11:26:02 -0700

    perfect

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,9 +1,15 @@
 package app;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import app.beans.MyFirstBean;
+import app.beans.Repo;
 import controller.AnyaController;
 
 @SpringBootApplication(scanBasePackages = { "app.services", "app.beans" })
@@ -12,6 +18,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(Repo repo) {
+		return args -> {
+			Stream.of("Rachel", "Monica", "Phoebe", "Joey", "Ross", "Chandler").forEach(name -> {
+				MyFirstBean bean = new MyFirstBean(name, 1990, 20L);
+				repo.save(bean);
+			});
+			repo.findAll().forEach(System.out::println);
+		};
 	}
 
 }
