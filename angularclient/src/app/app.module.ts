3dc896ed3a96a88b6aade14d2b5e44b1b022d798
anyaguuu <ag156@rice.edu>
2023-07-28 14:27:13 -0700

    small changes

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
-    UserFormComponent
+    UserListComponent
   ],
   imports: [
     BrowserModule
